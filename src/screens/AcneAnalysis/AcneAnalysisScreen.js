7011bffa4bec5aa032e33390f137d23996e9469e
jiyeon <bjyy0201@gmail.com>
2024-04-09 23:26:08 +0900

    기능 본문 코멘트 추가

--- src/screens/AcneAnalysis/AcneAnalysisScreen.js
+++ src/screens/AcneAnalysis/AcneAnalysisScreen.js
@@ -12,7 +12,7 @@ export default function App() {
     React.useCallback(() => {
       // 탭이 활성화될 때 실행되는 함수
       console.log('탭이 활성화되었습니다.');
-  
+
       // 탭이 비활성화될 때 실행되는 함수
       return () => {
         console.log('탭이 비활성화되었습니다.');
@@ -20,7 +20,7 @@ export default function App() {
       };
     }, [])
   );
-  
+
 
   const navigation = useNavigation();
 
@@ -38,14 +38,30 @@ export default function App() {
       </View>
       <View style={styles.subseperatorArea}>
         <View style={styles.subseperator}>
-      </View>
-      </View>
-      <View style={styles.ContentsArea}>
+        </View>
+        <View style={styles.commentArea}>
+          <Text style={[styles.textStyle, { fontSize: width * 25 }]}>
+            얼굴에 <Text style={{ fontWeight: 'bold' }}>트러블</Text>이 생겼나요?
+          </Text>
+          <Text style={[styles.textStyle, { fontSize: width * 20 }]}>
+            {'\n'}
+            여드름은 잘못 방치하거나 압출하면{'\n'}
+            염증이 심해지고 흉터가 남기도 합니다.{'\n'}
+            이를 해결하기 위해선 올바른 관리가 중요한데요.{'\n'}
+            {'\n'}
+            여드름도  <Text style={{ fontWeight: 'bold' }}>종류가 다양</Text>하고 각각마다  <Text style={{ fontWeight: 'bold' }}>관리법이{'\n'}
+            다르다</Text>는 사실 알고 계셨나요?
+            {'\n'}{'\n'}
+            저희가 <Text style={{ fontWeight: 'bold' }}>피부 진단 AI</Text>를 통해 어떤 유형의 여드름인지{'\n'}
+            확인하고 적절한 대처법을 알려드립니다.{'\n'}
+            편하고 빠르게 진단 후 관리법을 처방받아 보세요!
+          </Text>
 
+        </View>
       </View>
       <View style={styles.ButtonArea}>
-        <BasicButton color={colors.buttonBlue} onPress={goCamera} title={'사진 촬영'}/>
-        <BasicButton color={colors.buttonSkyBlue} onPress={goAlbum} title={'앨범에서 선택'}/>
+        <BasicButton color={colors.buttonBlue} onPress={goCamera} title={'사진 촬영'} />
+        <BasicButton color={colors.buttonSkyBlue} onPress={goAlbum} title={'앨범에서 선택'} />
       </View>
     </View>
   );
@@ -62,9 +78,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     height: height * 70,
   },
-  ContentsArea: {
-    flexDirection: 'row',
+  commentArea: {
+    flexDirection: 'columm',
     height: height * 500,
+    width: width * 400,
+    marginTop: height * 10,
+    // backgroundColor: 'red',
   },
   ButtonArea: {
     flexDirection: 'row',
@@ -72,15 +91,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     height: height * 100,
   },
-  subseperatorArea:{
+  subseperatorArea: {
     justifyContent: 'center',
-    alignItems:'center',
+    alignItems: 'center',
   },
-  subseperator:{
-    backgroundColor:'grey',
+  subseperator: {
+    backgroundColor: 'grey',
     width: width * 400,
-    height:1,
-    
+    height: 1,
+  },
+  textStyle: {
+    fontFamily: "NanumSquareRoundL",
+    color: styleG.textGrey,
   }
 
 });
