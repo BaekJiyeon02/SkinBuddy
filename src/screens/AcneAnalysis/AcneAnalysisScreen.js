aa2771bef6270e70d26e4248b58836ea42c4c28e
jiyeon <bjyy0201@gmail.com>
2024-04-11 22:25:32 +0900

    Ai 트러블 유형 화면 UI 구성 완성

--- src/screens/AcneAnalysis/AcneAnalysisScreen.js
+++ src/screens/AcneAnalysis/AcneAnalysisScreen.js
@@ -50,11 +50,23 @@ export default function App() {
             이를 해결하기 위해선 올바른 관리가 중요한데요.{'\n'}
             {'\n'}
             여드름도  <Text style={styleG.textBold}>종류가 다양</Text>하고 각각마다 <Text style={styleG.textBold}>관리법이{'\n'}
-            다르다</Text>는 사실 알고 계셨나요?
+              다르다</Text>는 사실 알고 계셨나요?
             {'\n'}{'\n'}
             저희가 <Text style={styleG.textBold}>피부 진단 AI</Text>를 통해 어떤 유형의 여드름인지{'\n'}
             확인하고 적절한 대처법을 알려드립니다.{'\n'}
             편하고 빠르게 진단 후 관리법을 처방받아 보세요!
+          </Text>
+          <Text style={[styleG.textStyle, { fontSize: width * 18, marginTop: height * 30 }]}>
+            이렇게 찍어주세요 :){'\n'}
+            {'\t'}• <Text style={{color:colors.highlightBlue}}>화장하지 않은</Text> 맨 얼굴{'\n'}
+            {'\t'}• <Text style={{color:colors.highlightBlue}}>선명하게</Text> 찍은 얼굴{'\n'}
+            {'\t'}• <Text style={{color:colors.highlightBlue}}>트러블이 있는 부위</Text>의 얼굴{'\n'}
+            {'\n'}
+            이렇게 찍으면 어려워요 :({'\n'}
+            {'\t'}• <Text style={{color:colors.highlightRed}}>초점이 맞지 않는</Text> 얼굴{'\n'}
+            {'\t'}• <Text style={{color:colors.highlightRed}}>너무 먼 거리에서</Text> 찍은 얼굴{'\n'}
+            {'\t'}• <Text style={{color:colors.highlightRed}}>옆모습이나 각도가 기운 채</Text>로 찍은 얼굴{'\n'}
+            {'\t'}• <Text style={{color:colors.highlightRed}}>흔들리게</Text> 찍힌 얼굴{'\n'}
           </Text>
 
         </View>
@@ -82,7 +94,7 @@ const styles = StyleSheet.create({
     flexDirection: 'columm',
     height: height * 500,
     width: width * 400,
-    marginTop: height * 10,
+    marginTop: height * 20,
     // backgroundColor: 'red',
   },
   ButtonArea: {
