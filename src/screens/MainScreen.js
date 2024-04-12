0fa029b7d511ae703f191b337ac656683b57c0be
jiyeon <bjyy0201@gmail.com>
2024-04-12 15:29:21 +0900

    크기 비율 조정

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -7,22 +7,44 @@ import { styleG, colors, width, height } from '../assets/globalStyles';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
+
 export default function App() {
 
+
+  
   const [backColor, setBackColor] = useState(colors.buttonSkyBlue);
   const [userMbti, setUserMbti] = useState('DRPT');
+  const [doResult,setDoResult]=useState(0);
 
+
+  useEffect(() => {
+    // 컴포넌트가 마운트된 후 실행되는 부분
+    ScoreCalcDO(); // 함수 호출
+  }, [doResult] //doResult 값이 변경될때마다 로그
+  );
+  
+  function ScoreCalcDO(){
+    const doScore=27;
+    if(doScore>=26){
+      const score=15*(doScore/17);
+      setDoResult(score)
+      console.log(doResult)
+    }
+  }
+  
+  
   return (
-
     <View style={styles.container}>
       <View style={styles.adBanner}>
         <AdBanner />
       </View>
       <View style={styles.bottom}>
         <View style={styles.userNameArea}>
-          {/* 유저 이름 받아 올시 해제
-        <Text>{userName}</Text> */}
-          <Text style={[styleG.textStyle, { fontSize: width * 23, color: colors.textGrey, fontFamily: 'NanumSquareRoundB', fontWeight: '100' }]}><Text style={[styleG.textBold, { fontSize: width * 30 }]}>박정재</Text>님의 피부 MBTI</Text>
+          <Text style={[styleG.textStyle, { fontSize: width * 23, color: colors.textGrey, fontFamily: 'NanumSquareRoundB', fontWeight: '100' }]}><Text style={[styleG.textBold, { fontSize: width * 30 }]}>
+          {/* 유저 이름 받아 온 후 변경
+          {userName}*/}
+            백지연
+            </Text>님의 피부 MBTI</Text>
         </View>
         {/* MBTI 결과 그래프 */}
         <View style={styles.mbtiResult}>
@@ -32,8 +54,8 @@ export default function App() {
             </TouchableOpacity>
           </View>
           <View style={styles.graph}>
-            <MbtiGraph category={'DO'} score={130}></MbtiGraph>
-            <MbtiGraph category={'RS'} score={130}></MbtiGraph>
+            <MbtiGraph category={'DO'} score={135}></MbtiGraph>
+            <MbtiGraph category={'RS'} score={135}></MbtiGraph>
             <MbtiGraph category={'PN'} score={130}></MbtiGraph>
             <MbtiGraph category={'WT'} score={130}></MbtiGraph>
           </View>
@@ -55,7 +77,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     backgroundColor: colors.softGrey,
     borderColor: colors.darkGrey,
-    borderWidth: 3,
+    borderWidth: width * 3,
     borderRadius: 15,
 
   },
@@ -84,9 +106,9 @@ const styles = StyleSheet.create({
     height: height * 350,
   },
   userNameArea: {
-    height: 40,
-    width: 400,
-    paddingLeft: 10,
+    height: width * 40,
+    width: width * 400,
+    paddingLeft: width * 10,
   },
   bottom:{
 
