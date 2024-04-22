2ccafe81b065bd9318d22fa3d3af65347df7503e
jiyeon <bjyy0201@gmail.com>
2024-04-22 23:05:20 +0900

    회원가입 구현 완료

--- src/screens/Login/JoinScreen.js
+++ src/screens/Login/JoinScreen.js
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import InputTextBox from '../../components/InputTextBox';
 import { styleG, colors, width, height } from '../../assets/globalStyles'
@@ -13,7 +13,7 @@ import axios from 'axios';
 export default function JoinScreen() {
   const navigation = useNavigation();
   const joinApiUrl = 'http://52.79.237.164:3000/user/create';
-  
+
 
   // 입력 필드의 값을 추적하기 위해 useState 훅을 사용.
   const [inputId, setInputId] = useState('');
@@ -21,32 +21,32 @@ export default function JoinScreen() {
   const [inputPwCheck, setInputPwCheck] = useState('');
   const [inputNickname, setInputNickname] = useState('');
   const [inputTel, setInputTel] = useState('');
-  
+
   const [ableId, setAbleId] = useState(false);
-  
+
   const [idTextColor, setIdTextColor] = useState(colors.textGray);
   const [pwTextColor, setPwTextColor] = useState(colors.textGray);
   const [pwCheckTextColor, setPwCheckTextColor] = useState(colors.textGray);
   const [nicknameTextColor, setNicknameTextColor] = useState(colors.textGray);
   const [telTextColor, setTelTextColor] = useState(colors.textGray);
 
-  const [buttonColor, setButtonColor] =useState(colors.gray);
-  const [buttonAble, setBttonAble] =useState(true);
+  const [buttonColor, setButtonColor] = useState(colors.gray);
+  const [buttonAble, setBttonAble] = useState(true);
 
   useEffect(() => {
-    if(idTextColor == colors.highlightBlue && pwTextColor == colors.highlightBlue && pwCheckTextColor == colors.highlightBlue && nicknameTextColor == colors.highlightBlue && telTextColor == colors.highlightBlue){
+    if (idTextColor == colors.highlightBlue && pwTextColor == colors.highlightBlue && pwCheckTextColor == colors.highlightBlue && nicknameTextColor == colors.highlightBlue && telTextColor == colors.highlightBlue) {
       setButtonColor(colors.loginBlue);
       setBttonAble(false);
     }
-    else{
+    else {
       setButtonColor(colors.gray);
       setBttonAble(true);
     }
 
-  },[idTextColor, pwTextColor, telTextColor, pwCheckTextColor, nicknameTextColor, telTextColor])
+  }, [idTextColor, pwTextColor, telTextColor, pwCheckTextColor, nicknameTextColor, telTextColor])
 
   function is_id(asValue) {
-    var regExp = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{5,19}$/;
+    var regExp = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{6,19}$/;
     return regExp.test(asValue);
   }
   function is_pw(asValue) {
@@ -54,7 +54,7 @@ export default function JoinScreen() {
     return regExp.test(asValue);
   }
   function is_nickname(asValue) {
-    var regExp = /^.{1,20}$/;
+    var regExp = /^.{1,8}$/;
     return regExp.test(asValue);
   }
   function is_tel(asValue) {
@@ -76,7 +76,7 @@ export default function JoinScreen() {
   };
 
   //pw 조건 Check part
-  const handlePwValue=(text)=> {
+  const handlePwValue = (text) => {
     setInputPw(text);
     if (is_pw(text)) {
       setPwTextColor(colors.highlightBlue);
@@ -85,20 +85,20 @@ export default function JoinScreen() {
       setPwTextColor(colors.highlightRed);
     }
   }
-  
+
   //pwCheck 조건 Check part
-  const handlePwCheckValue=(text)=> {
+  const handlePwCheckValue = (text) => {
     setInputPwCheck(text);
-    if (inputPw==text) {
+    if (inputPw == text) {
       setPwCheckTextColor(colors.highlightBlue);
     }
-    else if (!(inputPw==text)) {
+    else if (!(inputPw == text)) {
       setPwCheckTextColor(colors.highlightRed);
     }
   }
 
   //nickname 조건 Check part
-  const handleNicknameValue=(text)=> {
+  const handleNicknameValue = (text) => {
     setInputNickname(text);
     if (is_nickname(text)) {
       setNicknameTextColor(colors.highlightBlue);
@@ -107,9 +107,9 @@ export default function JoinScreen() {
       setNicknameTextColor(colors.highlightRed);
     }
   }
-  
+
   //tel 조건 Check part
-  const handleTelValue=(text)=> {
+  const handleTelValue = (text) => {
     setInputTel(text);
     if (is_tel(text)) {
       setTelTextColor(colors.highlightBlue);
@@ -122,21 +122,42 @@ export default function JoinScreen() {
   const goJoin = () => {
 
     const postData = {
-      "userId" : inputId,
-      "psword" : inputPw,
-      "nickname" : inputNickname,
-      "tel" : inputTel
-  };
+      "userId": inputId,
+      "psword": inputPw,
+      "nickname": inputNickname,
+      "tel": inputTel
+    };
 
     axios.post(joinApiUrl, postData)
-    .then(response => {
+      .then(response => {
         // 요청 성공 시 처리
         console.log('Data:', response.data);
-    })
-    .catch(error => {
+        if (response.data['property'] == 200) {
+          Alert.alert(
+            response.data['message'],
+            '',
+            [
+              { text: '확인' },
+            ],
+            { cancelable: false } // 경고 창을 취소할 수 없는 경우 설정합니다.
+          );
+          navigation.navigate('Login');
+        }
+        else if (response.data['property'] == 302) {
+          Alert.alert(
+            response.data['message'],
+            '',
+            [
+              { text: '확인' },
+            ],
+            { cancelable: false } // 경고 창을 취소할 수 없는 경우 설정합니다.
+          );
+        }
+      })
+      .catch(error => {
         // 요청 실패 시 처리
         console.error('Error:', error);
-    });
+      });
   }
   return (
 
@@ -150,19 +171,19 @@ export default function JoinScreen() {
           <View style={{ height: height * 425 }}>
             <View style={styles.inputArea}>
               <InputTextBox title={'ID'} value={'inputId'} onChangeText={handleIdValue} />
-              <Text style={{ color: idTextColor, fontSize: width*13 }}>• 아이디를 입력해주세요{'\n'}(영문+숫자, 6글자 이상, 20글자 이하)</Text>
-              <InputTextBox title={'PW'} value={'inputPw'} onChangeText={handlePwValue} type={'password'}/>
-              <Text style={{ color: pwTextColor, fontSize: width*13 }}>• 비밀번호를 입력해주세요{'\n'}(영문+숫자+특수기호, 8글자 이상, 20글자 이하)</Text>
-              <InputTextBox title={'PW Check'} value={'inputPwCheck'} onChangeText={handlePwCheckValue} type={'password'}/>
-              <Text style={{ color: pwCheckTextColor, fontSize: width*13 }}>• 입력하신 비밀번호를 다시 입력해주세요)</Text>
-              <InputTextBox title={'Nickname'} value={'inputNickname'} onChangeText={handleNicknameValue}/>
-              <Text style={{ color: nicknameTextColor, fontSize: width*13 }}>• 사용하실 닉네임을 입력해주세요 (20글자 이하)</Text>
+              <Text style={{ color: idTextColor, fontSize: width * 13 }}>• 아이디를 입력해주세요{'\n'}(영문+숫자, 6글자 이상, 20글자 이하)</Text>
+              <InputTextBox title={'PW'} value={'inputPw'} onChangeText={handlePwValue} type={'password'} />
+              <Text style={{ color: pwTextColor, fontSize: width * 13 }}>• 비밀번호를 입력해주세요{'\n'}(영문+숫자+특수기호, 8글자 이상, 20글자 이하)</Text>
+              <InputTextBox title={'PW Check'} value={'inputPwCheck'} onChangeText={handlePwCheckValue} type={'password'} />
+              <Text style={{ color: pwCheckTextColor, fontSize: width * 13 }}>• 입력하신 비밀번호를 다시 입력해주세요)</Text>
+              <InputTextBox title={'Nickname'} value={'inputNickname'} onChangeText={handleNicknameValue} />
+              <Text style={{ color: nicknameTextColor, fontSize: width * 13 }}>• 사용하실 닉네임을 입력해주세요 (20글자 이하)</Text>
               <InputTextBox title={'Tel'} value={'inputTel'} onChangeText={handleTelValue} />
-              <Text style={{ color: telTextColor, fontSize: width*13 }} >• 본인 전화번호의 숫자만 입력해주세요</Text>
+              <Text style={{ color: telTextColor, fontSize: width * 13 }} >• 본인 전화번호의 숫자만 입력해주세요</Text>
             </View>
           </View>
           <View style={styles.buttonArea}>
-            <BasicButton color={buttonColor} size={300} title={"회원가입"} onPress={goJoin} able={buttonAble}/>
+            <BasicButton category={'SideMargin'} color={buttonColor} size={300} title={"회원가입"} onPress={goJoin} able={buttonAble} />
           </View>
         </View>
       </KeyboardAwareScrollView >
