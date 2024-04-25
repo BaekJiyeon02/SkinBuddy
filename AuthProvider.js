f25cb0b0ba9149fa53b7270390be749d60696d26
jiyeon <bjyy0201@gmail.com>
2024-04-25 22:22:12 +0900

    비밀번호 변경 / 계정 탈퇴 구현 완료

--- AuthProvider.js
+++ AuthProvider.js
@@ -1,33 +1,44 @@
 import React, { createContext, useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
 import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import * as Updates from 'expo-updates';
 
 
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+
+
     const [userId, setUserId] = useState(null);
-    const [userName, setUserName] =useState(null);
-    const [skinType, setSkinType] =useState('????');
+    const [userName, setUserName] = useState(null);
+    const [skinType, setSkinType] = useState('????');
+    const [doScore, setDoScore] = useState(0);
+    const [rsScore, setRsScore] = useState(0);
+    const [pnScore, setPnScore] = useState(0);
+    const [wtScore, setWtScore] = useState(0);
     const [loading, setLoading] = useState(true);
 
-    const profileUrl='http://52.79.237.164:3000/user/home/profile'
-    
+    const profileUrl = 'http://52.79.237.164:3000/user/home/profile';
+    const deleteUrl = 'http://52.79.237.164:3000/user/delete';
+    const changeUrl = 'http://52.79.237.164:3000/user/change/psword';
+
 
     // AsyncStorage에서 로그인 상태와 사용자 아이디를 불러오는 함수
     const loadAuthData = async () => {
         setLoading(true)
         try {
             const storedUserId = await AsyncStorage.getItem('userId');
-            console.log('userId:',storedUserId)
-            if (storedUserId!==null) {
+            console.log('userId:', storedUserId)
+            if (storedUserId !== null) {
                 setUserId(storedUserId);
                 profileCall(storedUserId)
             }
-            else if(storedUserId==null){
+            else if (storedUserId == null) {
                 setUserId(false)
-                
+
             }
         } catch (error) {
             console.error('Failed to load user ID from AsyncStorage:', error);
@@ -46,6 +57,7 @@ export const AuthProvider = ({ children }) => {
         } catch (error) {
             console.error('Failed to store user ID in AsyncStorage:', error);
         }
+        setLoading(false);
     };
 
     // 로그아웃 상태를 업데이트하는 함수
@@ -57,46 +69,133 @@ export const AuthProvider = ({ children }) => {
             console.error('Failed to remove user ID from AsyncStorage:', error);
         }
     };
-    // 프로필을 불러오는 함수
-    const profileCall = async(id)=>{
+
+    //계정 탈퇴 함수
+    const deleteAccount = async(pwd) => {
+        const userPwd = pwd
+        const postData = {
+            "userId": userId,
+            "psword": userPwd
+        };
+        await axios.delete(deleteUrl,  { data: postData })
+            .then(response => {
+                // 요청 성공 시 처리
+                console.log(response.data)
+                console.log(postData)
+                if (response.data['property'] == 200) {
+                    logout();
+                    Updates.reloadAsync();
+                }
+                else if (response.data['property'] == 301) {
+                    console.log(response.data)
+                    Alert.alert(
+                        response.data['message'],
+                        '',
+                        [
+                            { text: '확인' },
+                        ],
+                    );
+                }
+            })
+            .catch(error => {
+                // 요청 실패 시 처리
+                console.log(error)
+            })
+
+    }
+
+    //비밀번호 변경 함수
+    const changePassword = (pwd, newPwd) => {
 
         const postData = {
-            "userId" : id,
+            "userId": userId,
+            "psword": pwd,
+            "newPsword": newPwd
         };
-          axios.post(profileUrl, postData)
-          .then(response => {
-              // 요청 성공 시 처리
-              console.log(response.data)
-              profile(response.data)
-          })
-          .catch(error => {
-              // 요청 실패 시 처리
-              console.error('Failed to load user profile:', error);
+        console.log(postData)
+        axios.put(changeUrl, postData)
+            .then(response => {
+                // 요청 성공 시 처리
+                if (response.data['property'] == 200) {
+                    
+                    Alert.alert(
+                        '변경되었습니다',
+                        '',
+                        [
+                            { text: '확인', onPress: async () => {
+                                Updates.reloadAsync();
+                          }}
+                        ],
+                      );
+                }
+                else if (response.data['property'] == 301) {
+                    console.log(response.data)
+                    Alert.alert(
+                        response.data['message'],
+                        '',
+                        [
+                            { text: '확인' },
+                        ],
+                    );
+                }
+            })
+            .catch(error => {
+                // 요청 실패 시 처리
+                console.error('Failed to change user password:', error);
             });
-        }
-        //프로필 상태를 업데이트 하는 함수
-        const profile = async(data)=>{
-            try {
-                console.log('data: ',data)
-                setUserName(data['nickname']);
-                if(data['skinType']!==null){
-                    await AsyncStorage.setItem('skinType',data['skinType']);
-                    await AsyncStorage.setItem('oliyScore',data['oliyScore']);
-                    await AsyncStorage.setItem('resistanceScore',data['resistanceScore']);
-                    await AsyncStorage.setItem('non_pigmentScore',data['non_pigmentScore']);
-                }
-                else if(data['skinType']==null){
-                    setSkinType('????')
-                }
 
-            }catch(error){
-                console.error('Failed to send user profile:', error);
+
+    }
+    // 프로필을 불러오는 함수
+    const profileCall = async (id) => {
+
+        const postData = {
+            "userId": id,
+        };
+        axios.post(profileUrl, postData)
+            .then(response => {
+                // 요청 성공 시 처리
+                console.log(response.data)
+                profile(response.data)
+            })
+            .catch(error => {
+                // 요청 실패 시 처리
+                console.error('Failed to load user profile:', error);
+            });
+    }
+    //프로필 상태를 업데이트 하는 함수
+    const profile = async (data) => {
+        setLoading(true)
+        try {
+            console.log('data: ', data)
+            setUserName(data['nickname']);
+            if (data['skinType'] !== null) {
+
+                await AsyncStorage.setItem('skinType', data['skinType']);
+
+                setDoScore(data['oilyScore'])
+                setRsScore(data['resistanceScore'])
+                setPnScore(data['non_pigmentScore'])
+                setWtScore(data['tightScore'])
+                setSkinType(data['skinType'])
+                setLoading(false);
+
             }
-            setLoading(false);
+            else if (data['skinType'] == null) {
+                setSkinType('????')
+            }
+        } catch (error) {
+            console.error('Failed to send user profile:', error);
+        }
+        setLoading(false);
+    }
+
+    const refreshMbti = () => {
+        profileCall(userId)
     }
 
     return (
-        <AuthContext.Provider value={{ userId, login, logout, loading, userName, skinType }}>
+        <AuthContext.Provider value={{ userId, login, logout, loading, userName, skinType, doScore, rsScore, pnScore, wtScore, changePassword, refreshMbti, deleteAccount }}>
             {children}
         </AuthContext.Provider>
     );
