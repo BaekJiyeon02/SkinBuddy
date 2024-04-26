326e84827752c9e4bb7391020c7ead817f3a5ca2
jiyeon <bjyy0201@gmail.com>
2024-04-26 22:43:57 +0900

    버전 정보 구현

--- src/screens/Setting/VersionInformationScreen.js
+++ src/screens/Setting/VersionInformationScreen.js
@@ -1,12 +1,13 @@
 
 import React,{useState, useEffect, useRef} from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
+import { styleG, colors, width, height } from '../../assets/globalStyles';
 
-export default function App() {
+export default function VersionInformationScreen() {
 
   return (
     <View style={styles.container}>
- 
+      <Text style={[styleG.textBold, {fontSize:width * 20}]}>버전: V.0.1</Text>
     </View>
   );
 }
@@ -15,6 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: 'white',
     paddingBottom: 20
   },
