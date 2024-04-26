508967469aec8b22245bbb661ec6d3d9b5588b7c
jiyeon <bjyy0201@gmail.com>
2024-04-26 22:43:50 +0900

    이용약관 구현

--- src/screens/Setting/TermsOfUseScreen.js
+++ src/screens/Setting/TermsOfUseScreen.js
@@ -1,12 +1,36 @@
 
-import React,{useState, useEffect, useRef} from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { styleG, colors, width, height } from '../../assets/globalStyles';
+import axios from 'axios';
 
-export default function App() {
+export default function TermsOfUseScreen() {
+
+  const termsUrl = "http://52.79.237.164:3000/user/terms"
+
+  const [termsList, setTermsList] = useState([]);
+  useEffect(()=>{
+    getTerms()
+
+  },[])
+
+  function getTerms(){
+    axios.get(termsUrl)
+    .then(response => {
+      console.log('응답 데이터:', response.data);
+      setTermsList(response.data["trems"]);
+    })
+    .catch(error => {
+      console.error('에러 발생:', error);
+    });
+}
+
 
   return (
     <View style={styles.container}>
- 
+      <Text>
+      {termsList}
+      </Text>
     </View>
   );
 }
@@ -14,6 +38,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: width * 20,
     justifyContent: 'center',
     backgroundColor: 'white',
     paddingBottom: 20
