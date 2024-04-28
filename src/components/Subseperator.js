3d1ab7d378e828e291a4ab27f9434738270cb202
jiyeon <bjyy0201@gmail.com>
2024-04-28 23:01:32 +0900

    구분선 구현

--- src/components/Subseperator.js
+++ src/components/Subseperator.js
@@ -1,7 +1,13 @@
 import { StyleSheet, View } from "react-native"
-import { colors, width, height, styleG } from '../assets/globalStyles';
+import { colors, width, height } from '../assets/globalStyles';
 
-export default function Subseperator() {
+export default function Subseperator({type}) {
+    if(type=='thin'){
+
+        return (
+            <View style={styles.subseperatorThin}></View>
+        )
+    }
 
     return (
         <View style={styles.subseperator}></View>
@@ -10,8 +16,13 @@ export default function Subseperator() {
 const styles = StyleSheet.create({
 
     subseperator: {
-        backgroundColor: 'grey',
+        backgroundColor: colors.darkGray,
         width: width * 400,
-        height: 1,
+        height: height * 2,
+    },
+    subseperatorThin: {
+        backgroundColor: colors.darkGray,
+        width: width * 400,
+        height: height * 1,
     }
 })
