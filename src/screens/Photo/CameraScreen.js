5f675177da413ee32e0196e427bcb44610693e49
jiyeon <bjyy0201@gmail.com>
2024-05-03 21:54:32 +0900

    camera 사진 보내기 구현

--- src/screens/Photo/CameraScreen.js
+++ src/screens/Photo/CameraScreen.js
@@ -1,127 +1,158 @@
-
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import CameraButton from '../../components/CameraButton'
 import { colors, width, height } from '../../assets/globalStyles';
+import { useNavigation } from '@react-navigation/native';
+import { AuthContext } from '../../../AuthProvider';
+import axios from 'axios';
 
-export default function App() {
-    const [hasCameraPermission, setHasCamerPermission] = useState(null); //카메라 접근 권한 여부
-    const [image, setImage] = useState(null);
-    const [type, setType] = useState(Camera.Constants.Type.back); //카메라 후면/전면 설정. 기본값:후면
-    const [flash, setFlash] = useState(Camera.Constants.FlashMode.off) //카메라 플래쉬 설정. 기본값:꺼짐
-    const cameraRef = useRef(null);
-    const [zoomLevel, setZoomLevel] = useState(0);
+export default function CameraScreen({ route }) {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+  const { userId } = useContext(AuthContext);
+  const { category } = route.params;
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        (async () => {
-            //카메라 권한 얻어오기
-            MediaLibrary.requestPermissionsAsync();
-            const cameraStatus = await Camera.requestCameraPermissionsAsync();
-            setHasCamerPermission(cameraStatus.status == 'granted')
-        })();
-    }, [])
+  const AiTroubleUrl = 'http://ceprj.gachon.ac.kr:60017/test';
 
-    //사진 찍기 버튼 눌렀을때
-    const takePicture = async () => {
-        if (cameraRef) {
-            try {
-                const data = await cameraRef.current.takePictureAsync();
-                console.log(data);//사진에 대한 크기와 정보
-                setImage(data.uri);
-                console.log(data.uri)
-            } catch (e) {
-                console.log(e);
-            }
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status == 'granted');
+    })();
+  }, []);
+
+  const handleFocus = async () => {
+    if (cameraRef.current) {
+      await cameraRef.current.focus();
+    }
+  };
+
+  const handleTouchStart = async (event) => {
+    if (cameraRef.current) {
+      const touchX = event.nativeEvent.locationX;
+      const touchY = event.nativeEvent.locationY;
+      await cameraRef.current.focusAtPoint(touchX, touchY);
+    }
+  };
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        setImage(data.uri);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+  
+
+  const sendImage = async () => {
+    if (image) {
+      const screenName = category;
+      if (category == 'ImprovementAnalysisResultScreen') {
+        navigation.navigate(screenName);
+      } else {
+        const formData = new FormData();
+        formData.append('file', {
+          uri: image,
+          type: 'image/jpeg',
+          name: 'photo.jpeg',
+        });
+        formData.append('userId', userId);
+
+        axios.post(AiTroubleUrl, formData)
+          .then(response => {
+              console.log("response:",response["data"])
+              const recordId=response['data']['recordId']
+              const acneLevel=response['data']['acneLevel']
+              navigation.navigate("AiTroubleResultScreen",{recordId:recordId, acneLevel:acneLevel})
+              
+            // if (response['Property'] == 200) {
+            //   console.log('사진 보냄!');
+            // } else {
+            //   console.log('사진 실패!');
+            // }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+
+        setImage(null);
+      }
+    }
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      {!image ?
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+          autoFocus={true}
+          onTouchStart={handleTouchStart}
+        >
+          <View style={styles.cameraButtons}>
+            <CameraButton
+              icon={'retweet'}
+              onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)}
+            />
+            <CameraButton
+              icon={'flash'}
+              color={flash === Camera.Constants.FlashMode.off ? 'gray' : colors.darkGray}
+              onPress={() => setFlash(flash === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.on : Camera.Constants.FlashMode.off)}
+            />
+          </View>
+        </Camera>
+        :
+        <Image source={{ uri: image }} style={styles.camera} />
+      }
+      <View>
+        {image ?
+          <View style={styles.cameraButtons}>
+            <CameraButton title={"다시 찍기"} icon="retweet" onPress={() => setImage(null)} />
+            <CameraButton title={"선택 완료"} icon="check" onPress={sendImage} />
+          </View>
+          :
+          <View style={styles.takePictureButtonArea}>
+            <CameraButton size={50} color={'#4F4F4F'} icon='circle' onPress={takePicture} />
+          </View>
         }
-    }
-    const saveImage = async () => {
-        if (image) {
-            try {
-                await MediaLibrary.createAssetAsync(image);
-                alert('Picture save!');
-                setImage(null);
-            } catch (e) {
-                console.log(e)
-            }
-        }
-    }
-
-
-    //camera 허용 안되어있을시 띄워주기
-    if (hasCameraPermission === false) {
-        return <Text>No access to camera</Text>
-    }
-
-    return (
-        <View style={styles.container}>
-            {!image ?
-                //Image가 없을 시 카메라 띄워줌
-                <Camera
-                    style={styles.camera}
-                    type={type}
-                    flashMode={flash}
-                    ref={cameraRef}
-                    autoFocus={true}
-
-                >
-
-                    <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        padding: 20
-                    }}>
-                        <CameraButton icon={'retweet'} onPress={() => {
-                            setType(type === CameraType.back ? CameraType.front : CameraType.back)
-                        }} />
-                        <CameraButton icon={'flash'}
-                            color={flash === Camera.Constants.FlashMode.off ? 'gray' : colors.darkGray}
-                            onPress={() => {
-                                setFlash(flash === Camera.Constants.FlashMode.off
-                                    ? Camera.Constants.FlashMode.on
-                                    : Camera.Constants.FlashMode.off)
-                            }} />
-                    </View>
-                </Camera>
-                :
-                <Image source={{ url: image }} style={styles.camera} />
-            }
-            <View>
-                {image ?
-                    <View style={{
-                        flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        paddingHorizontal: 50
-                    }}>
-                        <CameraButton title={"다시 찍기"} icon="retweet" onPress={() => setImage(null)} />
-                        <CameraButton title={"선택 완료"} icon="check" onPress={saveImage} />
-                    </View>
-                    :
-                    <View style={styles.takePictureButtonArea}>
-                        <CameraButton size={50} color={'#4F4F4F'} icon='circle' onPress={takePicture} />
-                    </View>
-                }
-            </View>
-        </View>
-    );
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        paddingBottom: 20,
-        backgroundColor: 'white',
-    },
-    camera: {
-        width : width * 450,
-        height : height * 450,
-
-    },
-    takePictureButtonArea: {
-        marginBottom: 10,
-
-    }
-
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingBottom: 20,
+    backgroundColor: 'white',
+  },
+  camera: {
+    width: width * 450,
+    height: height * 450,
+  },
+  cameraButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20
+  },
+  takePictureButtonArea: {
+    marginBottom: 10,
+  }
 });
